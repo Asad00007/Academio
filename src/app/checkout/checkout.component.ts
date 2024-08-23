cc473a445ce409ad47f34247e2edb80f6bbaa753
Asad00007 <asadsalim752@gmail.com>
2024-08-23 17:49:21 +0500

    Added Checkout page

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CourseService } from '../courses.service';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrl: './checkout.component.css'
+  styleUrl: './checkout.component.css',
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit {
+  constructor(private courseService: CourseService) {
+    window.scrollTo(0, 0);
+  }
+  selectedItems: any[] = [];
+  ngOnInit(): void {
+    this.selectedItems = this.courseService.selectedCourses;
+  }
+  removeSelected(name: string) {
+    this.courseService.selectedCourses =
+      this.courseService.selectedCourses.filter((item) => item !== name);
+    this.selectedItems = this.courseService.selectedCourses;
+    this.courseService.selectedCoursesChanged.emit();
+  }
 
+  placeOrder() {
+    alert(
+      'Thank you for placing the order and getting to the end of this website. I hope you enjoyed your time here.'
+    );
+  }
 }
